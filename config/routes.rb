c0f98a8ffa30a0f62039e5f3242f773b3d76bdd8
sam <sampurna.chapagainnn@gmail.com>
2025-12-16 09:05:34 +0000

    routes for devise-admin and set root path to pages-home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :admins
   get "pages/home"
   get "pages/about"
   get "pages/services"
@@ -11,7 +12,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
   # Defines the root path route ("/")
-  # root "posts#index"
+
+  root "pages#home"
 end

fb4f11ffeb4f206cdc3b200e2048ef7235c05249
sam <sampurna.chapagainnn@gmail.com>
2025-12-20 22:51:27 +0000

    add destroy and edit methods

--- app/controllers/admin/enquiries_controller.rb
+++ app/controllers/admin/enquiries_controller.rb
@@ -1,6 +1,30 @@
 class Admin::EnquiriesController < ApplicationController
   before_action :authenticate_admin!
+  before_action :set_enquiry, only: [:edit, :update, :destroy]
   def index
     @enquiries=Enquiry.order(created_at: :desc)
+  end
+
+  def destroy
+    @enquiry.destroy
+    redirect_to admin_enquiries_path, notice: "Enquiry deleted seccessfully"
+  end
+
+  def edit
+  end
+
+  def update
+    if @enquiry.update(enquiry_params)
+      redirect_to admin_enquiries_path, notice: "Enquiry updated successfully"
+    end
+  end
+
+  private
+  def set_enquiry
+    @enquiry = Enquiry.find(params[:id])
+  end
+
+  def enquiry_params
+    params.require(:enquiry).permit(:status)
   end
 end

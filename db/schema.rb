4c43b3a90ede4a89c9a27a6316e4483f3e1db3ee
sam <sampurna.chapagainnn@gmail.com>
2025-12-21 08:36:22 +0000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_20_135053) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_20_233602) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -34,5 +34,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_20_135053) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0, null: false
+    t.datetime "preferred_date_time"
   end
 end
